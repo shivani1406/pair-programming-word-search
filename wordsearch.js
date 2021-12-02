0123b536395fdb6769691089eb76ed856b54edee
Shivani Sharma <shivani1406sharma@gmail.com>
2021-12-02 01:30:52 +0000

    new commit

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,33 @@
 const wordSearch = (letters, word) => { 
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+        if (l.includes(word)) 
+        {
+            //console.log(l);
+            return true
+        }
     }
+
+    const verticleJoin =[];
+    for (let i = 0; i < letters[0].length; i++) {
+        let col = [];
+        for (let j = 0; j < letters.length; j++) {
+          col.push(letters[j][i]);
+        }
+        verticleJoin .push(col.join(''));
+      }
+      
+    for (h of verticleJoin )
+    {    //console.log(h);
+        if (h.includes(word)) 
+        {
+            //console.log(h);
+            return true
+        }
+    }
+    return false;
+    //console.log(word);
+    console.log(horizontalJoin);
 }
 
 module.exports = wordSearch
